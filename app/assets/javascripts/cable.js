98ca87f1ee08e346d708b04c3d71716c4909ed47
valcwt <cauwet.valentin@gmail.com>
2019-07-09 00:26:02 +0200

    in progress

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,20 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+
+$(document).ready(function(){
+  const mouseX, mouseY;
+  const ww = $( window ).width();
+  const wh = $( window ).height();
+  const traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".title").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
+
