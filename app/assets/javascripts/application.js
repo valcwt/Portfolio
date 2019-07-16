75f502db6d201d88a13df3fbbbb81d3e672d429d
valcwt <cauwet.valentin@gmail.com>
2019-07-16 15:57:44 +0200

    in progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,4 @@ $(document).ready(function(){
 });
 
 
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+
