6e6d8e3f1616fe34145f3bddda7ee0d9a882de1f
valcwt <cauwet.valentin@gmail.com>
2019-08-06 11:58:39 +0200

    in progress resolv pb

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -13,18 +13,3 @@
 }).call(this);
 
 
-$(document).ready(function(){
-  const mouseX, mouseY;
-  const ww = $( window ).width();
-  const wh = $( window ).height();
-  const traX, traY;
-  $(document).mousemove(function(e){
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    traX = ((4 * mouseX) / 570) + 40;
-    traY = ((4 * mouseY) / 570) + 50;
-    console.log(traX);
-    $(".title").css({"background-position": traX + "%" + traY + "%"});
-  });
-});
-
